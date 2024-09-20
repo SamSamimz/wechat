42db8468138d847ff8cd1fa6556a5ee9a123e8da
SamSamimz <samsamim595@gmail.com>
2024-09-20 20:57:52 +0600

    defer()

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -11,15 +11,15 @@ class MessageController extends Controller
     //__Store messages,
     public function store(Request $request)
     {
-
-        // dd($request->all());
+        defer(function () use ($request) {
         $message = Message::create([
             'receiver_id' => $request->receiver_id,
-            'sender_id' => $request->sender_id,
-            'message' => $request->newMessage,
+            'sender_id'   => $request->sender_id,
+            'message'     => $request->newMessage,
         ]);
-        // dispath the event
-        event(new MessageSent($message));
-        return redirect()->route('chat', ['id' => $request->receiver_id]);
+            broadcast(new MessageSent($message))->toOthers();
+        });
+        // event(new MessageSent($message));
+        // return redirect()->route('chat', ['id' => $request->receiver_id]);
     }
 }
